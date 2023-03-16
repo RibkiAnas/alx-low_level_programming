14255b6846ec124a9b9301050678f394d63ac4b4
Anas Ribki <anasribki147@gmail.com>
2023-03-16 21:09:56 +0100

    Edit 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
-* main - A programme that prints a line to 
-the standard error
+* main - Entry point
 * Return: Return 1 (Success)
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"");
+	fputs("- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
