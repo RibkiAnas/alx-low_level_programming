41fb472784360968631d510bbbc91c8386d64bab
Anas Ribki <anasribki147@gmail.com>
2023-03-17 14:15:59 +0100

    Edit some error in 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,15 @@
 */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i; j < 10; j++)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			if (i != 9 || j != 9)
-			{
-				putchar(i + '0');
-				putchar(',');
-				putchar(' ');
-				putchar(j + '0');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
