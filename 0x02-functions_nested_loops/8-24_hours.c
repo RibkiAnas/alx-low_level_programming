758b4f5774f242fd9fe1b3f8ed7fde18577351a8
Anas Ribki <anasribki147@gmail.com>
2023-03-21 14:58:22 +0000

    Edit some error in 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,21 +7,21 @@ void jack_bauer(void)
 {
 	int hour1, hour2, minute1, minute2;
 
-	for (hour1 = 0; hour1 < 3; hour1++)
+	for (hour1 = 0; hour1 <= 2; hour1++)
 	{
-		for (hour2 = 0; hour2 < 4; hour2++)
+		for (hour2 = 0; hour2 <= 9; hour2++)
 		{
-			for (minute1 = 0; minute1 < 6; minute1++)
+			for (minute1 = 0; minute1 <= 5; minute1++)
 			{
-				for (minute2 = 0; minute2 < 10; minute2++)
+				for (minute2 = 0; minute2 <= 9; minute2++)
 				{
 					if (hour1 >= 2 && hour2 >= 4)
-						return;
-					_putchar(hour1 + '0');
-					_putchar(hour2 + '0');
-					_putchar(':');
-					_putchar(minute1 + '0');
-					_putchar(minute2 + '0');
+						break;
+					_putchar(hour1 + 48);
+					_putchar(hour2 + 48);
+					_putchar(58);
+					_putchar(minute1 + 48);
+					_putchar(minute2 + 48);
 					_putchar('\n');
 				}
 			}
