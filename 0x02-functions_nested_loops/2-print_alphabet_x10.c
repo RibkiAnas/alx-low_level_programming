268c52542edec24a809b6504509896de29ac1666
Anas Ribki <anasribki147@gmail.com>
2023-03-21 12:57:45 +0000

    Add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_alphabet - prints alphabet in lowercase
+*/
+void print_alphabet(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
