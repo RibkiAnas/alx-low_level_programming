8c9b95e5e9d4f307d0ab5339b4f73701aa691000
Anas Ribki <anasribki147@gmail.com>
2023-03-21 19:53:38 +0000

    Edit 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,11 +22,13 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(res + '0');
 				}
 				else if (res >= 10)
 				{
 					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
