180de06fce6e55f21349a7045e8bed42907aef4e
Anas Ribki <anasribki147@gmail.com>
2023-03-21 14:15:14 +0000

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+*/
+void jack_bauer(void)
+{
+	int hour1, hour2, minute1, minute2;
+
+	for (hour1 = 0; hour1 < 3; hour1++)
+	{
+		for (hour2 = 0; hour2 < 4; hour2++)
+		{
+			for (minute1 = 0; minute1 < 6; minute1++)
+			{
+				for (minute2 = 0; minute2 < 10; minute2++)
+				{
+					if (hour1 == 2 && hour2 == 4)
+						return;
+					_putchar(hour1 + '0');
+					_putchar(hour2 + '0');
+					_putchar(':');
+					_putchar(minute1 + '0');
+					_putchar(minute2 + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
