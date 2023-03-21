63be2c131b17fa75df3d21004b5e58bfce0d1581
Anas Ribki <anasribki147@gmail.com>
2023-03-21 12:52:15 +0000

    Edit some error in 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,6 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Return 0 (Success)
-*/
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
 * print_alphabet - prints alphabet in lowercase
 */
 void print_alphabet(void)
