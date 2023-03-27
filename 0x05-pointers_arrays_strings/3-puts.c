44589b89df22a1cc254b043ad52709d17b268a4a
Anas Ribki <anasribki147@gmail.com>
2023-03-27 16:48:33 +0000

    Edit some error in 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.h"
 
 /**
 * _puts - function that prints a string,
@@ -8,14 +7,9 @@
 */
 void _puts(char *str)
 {
-	int i = 0;
-
 	while (*str != '\0')
 	{
-		_putchar(*(str + i));
-		i++;
-		if (*str == '\0')
-			break;
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
