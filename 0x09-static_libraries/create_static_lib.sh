2a30d17b6eea9d16003c44642506c53bf05970e7
Anas Ribki <anasribki147@gmail.com>
2023-04-08 15:40:41 +0000

    Edit second task

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# Create an object file for each .c file
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
-
-# Create the static library from all object files
-ar -rc liball.a *.o
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c | ar -rc liball.a *.o
