50bdfd1af34f9e5c2e32bb9505662f55823deb7c
Anas Ribki <anasribki147@gmail.com>
2023-04-29 20:25:15 +0100

    Fix some betty style

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,10 +14,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *temp, *next_node;
-	
+
 	if (!head || !*head)
 		return (-1);
-
 	temp = *head;
 
 	if (index == 0)
