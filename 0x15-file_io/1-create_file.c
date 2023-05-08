1e99bf3e3ff7250b0070e273b36ab6c6074407e7
Anas Ribki <anasribki147@gmail.com>
2023-05-08 21:42:57 +0100

    Edit 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,15 +12,28 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fp;
+	int fd;
+	ssize_t bytes_written;
 
-	fp = fopen(filename, "w");
-	if (!fp)
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
 	if (text_content)
-		fprintf(fp, "%s", text_content);
-	
-	fclose(fp);
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <unistd.h>
+#include <string.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
