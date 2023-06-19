d6790e1c2d372a588ca6630b939bbaf3a4e1bef9
Anas Ribki <anasribki147@gmail.com>
2023-06-19 20:20:38 +0100

    Edit task libdynamic.so

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* _strpbrk - function that searches a string for any
+* of a set of bytes.
+* @s: pointer of string.
+* @accept: accept string.
+*
+* Return: Returns a pointer to the byte in s that
+* matches one of the bytes in accept, or NULL
+* if no such byte is found.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
