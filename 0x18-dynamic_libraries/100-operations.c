60925391a38c514fc22d85924130368022e39301
Anas Ribki <anasribki147@gmail.com>
2023-06-19 20:41:11 +0100

    Add task 100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+* add - function that adds two numbers
+* @a: number one
+* @b: number two
+*
+* Return: result
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - function that sub two numbers
+* @a: number one
+* @b: number two
+*
+* Return: result
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - function that mul two numbers
+* @a: number one
+* @b: number two
+*
+* Return: result
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - function that div two numbers
+* @a: number one
+* @b: number two
+*
+* Return: result
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - function that mod two numbers
+* @a: number one
+* @b: number two
+*
+* Return: result
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
