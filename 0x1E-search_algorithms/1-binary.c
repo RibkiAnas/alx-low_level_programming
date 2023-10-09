252b3e932bed95fbd8894f4d2fba67fb20e2c62d
Anas Ribki <anasribki147@gmail.com>
2023-10-09 20:23:55 +0100

    Add task 1. Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to be printed.
+ * @size: Number of elements in array.
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - Searches for a value
+ * in a sorted array of integers using
+ * the Binary search algorithm.
+ * @array: Pointer to the first element of
+ * the array to search in.
+ * @size: Number of elements in array.
+ * @value: Value to search for
+ *
+ * Return: Index where @value is located,
+ * or -1 if not found or array is NULL.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, mid, right;
+
+	if (!array)
+		return (-1);
+
+	left = 0;
+	right = size - 1;
+	while (left <= right)
+	{
+		print_array(array + left, right - left + 1);
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
